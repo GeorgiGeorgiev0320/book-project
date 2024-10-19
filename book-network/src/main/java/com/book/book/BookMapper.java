9eeaed1c38e7f959198e50e587cb6b5ab0603cc5
GeorgiGeorgiev0320 <georgi.georgiev0320@gmail.com>
2024-10-19 22:13:49 +0300

    implement Book services

---
+++ book-network/src/main/java/com/book/book/BookMapper.java
@@ -0,0 +1,19 @@
+package com.book.book;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookMapper {
+    public Book toBook(BookRequest request) {
+        return Book.builder()
+                .id(request.id())
+                .title(request.title())
+                .authorName(request.authorName())
+                .isbn(request.isbn())
+                .synopsis(request.synopsis())
+                .shareable(request.shareable())
+                .archived(false)
+                .build();
+    }
+}
